af06da6fc141ebbbb931b357256152cb5e21828d
installer <installer@gmail.com>
2025-01-17 14:17:28 -0600

    gtk theme update

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -4,26 +4,42 @@
   inputs,
   ...
 }: {
+  catppuccin.gtk = {
+    enable = true;
+    flavor = "mocha";
+    accent = "blue";
+    size = "standard";
+
+    gnomeShellTheme = true;
+    tweaks = ["normal"];
+    icon = {
+      enable = true;
+      flavor = "mocha";
+      accent = "blue";
+    };
+  };
   gtk = {
-    iconTheme = {
-      name = "Papirus-Dark";
-      packages = pkgs.papirus-icon-theme;
-    };
-    theme.packages = pkgs.catppuccin-gtk.override {
-      accents = ["dark"]; # You can specify multiple accents here to output multiple themes
-      size = "standard";
-      variant = "mocha";
-    };
-    theme.name = "catppuccin-Dark";
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 14;
-    };
+    enable = true;
+
+    # iconTheme = {
+    #   name = "Papirus-Dark";
+    #   packages = pkgs.papirus-icon-theme;
+    # };
+    # theme.packages = pkgs.catppuccin-gtk.override {
+    #   accents = ["dark"]; # You can specify multiple accents here to output multiple themes
+    #   size = "standard";
+    #   variant = "mocha";
+    # };
+    #     theme.name = "catppuccin-mocha-blue";
+    #     gtk3.extraConfig = {
+    #       gtk-application-prefer-dark-theme = 1;
+    #     };
+    #     gtk4.extraConfig = {
+    #       gtk-application-prefer-dark-theme = 1;
+    #     };
+    #     font = {
+    #       name = "JetBrainsMono Nerd Font";
+    #       size = 14;
+    #     };
   };
 }
